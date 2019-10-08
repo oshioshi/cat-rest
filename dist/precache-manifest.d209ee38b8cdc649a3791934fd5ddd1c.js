e360c4de298d84788ff8340b9d19064f6111a49b
oshioshi <y.oshita0110@gmail.com>
2019-10-08 21:39:47 +0900

    first build

---
+++ dist/precache-manifest.d209ee38b8cdc649a3791934fd5ddd1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d83d49081303d7274083",
+    "url": "/js/chunk-vendors.0284c43b.js"
+  },
+  {
+    "revision": "eb8fd6d2b8b67aae2b5e",
+    "url": "/js/app.dd935c66.js"
+  },
+  {
+    "revision": "78015612dd2b9659a46b",
+    "url": "/js/about.266d1115.js"
+  },
+  {
+    "revision": "fab127db64bc816f1f9718a995173547",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "eb8fd6d2b8b67aae2b5e",
+    "url": "/css/app.dde1df84.css"
+  }
+];
