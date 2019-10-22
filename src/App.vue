e2e98be8e644e34cc8c1533022f01ee35e367d91
oshioshi <y.oshita0110@gmail.com>
2019-10-22 23:25:44 +0900

    get_tweet

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <div id="top">
+      <img id="topImg" alt="topImg" src="./assets/adorable-animal-baby-256632.jpg">
+    </div>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/topCat">殿堂入り</router-link> |
-      <router-link to="/about">マイナー猫</router-link> |
-      <router-link to="/about">猫種</router-link> |
-      <router-link to="/about">今日のおすすめ</router-link>
-      <img alt="Vue logo" src="./assets/logo_catRest.png">
+      
+      <router-link to="/">
+        <img alt="Vue logo" src="./assets/logo_catRest.png">
+      </router-link>
+      
+      <span>
+      <router-link to="/topCat">ラグドール</router-link> |
+      <router-link to="/about">マンチカン</router-link> |
+      <router-link to="/about">スコティッシュフォールド</router-link> |
+      <router-link to="/about">ノルウェージャンフォレストキャット</router-link> |
+      <router-link to="/about">メインクーン</router-link>
+      </span>
     </div>
     <router-view/>
   </div>
@@ -33,4 +41,30 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+body {
+  margin:0px;
+}
+
+#top {
+  height: 80vh;
+}
+
+#topImg {
+    width: 100vw;
+    height: 80vh;
+    object-fit: cover;
+    object-position: 0% 75%;
+}
+#nav {
+  background-color: #bbe0d9;
+  width: 100vw;
+  height: 20vh;
+  padding: 0px;
+  object-fit: cover;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
 </style>
--- src/views/topCat.vue
+++ src/views/topCat.vue
@@ -1,5 +1,61 @@
+
 <template>
   <div class="topCat">
-    <h1>おすすめ猫ランキング</h1>
+    <h1>ラグドール</h1>
+      <section v-if="hasError">
+        Error.
+      </section>
+      <section v-else>
+        <div v-if="loading">
+          Loading ...
+        </div>
+        <div v-else>
+          <ul>
+            <li v-for="tw in list" :key="tw">
+              <Tweet :id="tw.id_str" :options="{ align: 'center' }"/>
+            </li>
+          </ul>
+        </div>
+      </section>
   </div>
+  
 </template>
+<style>
+ul{
+  list-style: none;
+}
+</style>
+
+<script>
+import axios from 'axios'
+import {Tweet} from 'vue-tweet-embed'
+
+export default{
+  name: 'topCat',
+  components: {
+    Tweet: Tweet
+  },
+  data() {
+    return{
+      bpi: null,
+      hasError: false,
+      loading: true
+    }
+  },
+  mounted() {
+    axios.get('https://wc44116dwf.execute-api.us-east-1.amazonaws.com/default/getTweets')
+      .then(function(response) {
+        console.log(response.data.body.statuses)
+        this.list = response.data.body.statuses
+      }.bind(this))
+      .catch(function(error) {
+        console.log(error)
+        this.hasError = true
+      }.bind(this))
+      .finally(function() {
+        this.loading = false
+      }.bind(this))
+  }
+}
+
+</script>
